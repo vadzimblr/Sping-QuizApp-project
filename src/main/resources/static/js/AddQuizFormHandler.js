44a27d5530625a66700f7e3d21ba9dce3455a047
vadzimblr <vadim.poznyak.wrk@gmail.com>
2023-11-21 23:20:55 +0300

    Implementation of functionality to collect data from form to json string

--- src/main/resources/static/js/AddQuizFormHandler.js
+++ src/main/resources/static/js/AddQuizFormHandler.js
@@ -4,104 +4,126 @@ $(document).ready(() => {
 
     function addQuestion() {
         questionCounter++;
-        const questionDiv = $(document.createElement('div')).attr("class","form-group").html(`
-            
-            <div class="form-group">
-                <label  for="question${questionCounter}">Question ${questionCounter}:</label>
-                <input type="text" class="form-control" data-question="${questionCounter}" required>
-            </div>
-            <div class="form-group">
+        const questionDiv = $(`
+            <div class="form-group" data-question="${questionCounter}">
+                <label for="question${questionCounter}">Question ${questionCounter}:</label>
+                <input type="text" class="form-control" data-name-question required>
                 <label for="question${questionCounter}">Clarifying information to the question (optional):</label>
-                <textarea class="form-control" data-question="${questionCounter}" cols="30" rows="10"></textarea>
-            </div>
-            <div class="form-group">
-                <label for="answers${questionCounter}">Answers:</label>
-                <div class="answerContainer" data-answer-for-question="${questionCounter}">
-                    <div data-answer="0">
-                        <div class="form-group">
-                            <label for="">Answer 1:</label>
-                            <input type="text" class="form-control" required>
+                <textarea class="form-control" data-description-question cols="30" rows="10"></textarea>
+                <div class="form-group">
+                    <label for="answers${questionCounter}">Answers:</label>
+                    <div class="answerContainer" data-answer-for-question="${questionCounter}">
+                        <div data-answer="0">
+                            <div class="form-group">
+                                <label for="">Answer 1:</label>
+                                <input type="text" class="form-control" data-answer-name required>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Is correct?</label>
+                                <select class="form-control" name="isCorrect[]">
+                                    <option value="false">No</option>
+                                    <option value="true">Yes</option>
+                                </select>
+                            </div>
                         </div>
-                        <div class="form-group">
-                         <label for="">Is correct?</label>
-                          <select class="form-control" name="isCorrect[]" >
-                            <option value="false">No</option>
-                            <option value="true">Yes</option>
-                        </select> 
+                        <div data-answer="1">
+                            <div class="form-group">
+                                <label for="">Answer 2:</label>
+                                <input type="text" class="form-control" data-answer-name required>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Is correct?</label>
+                                <select class="form-control" name="isCorrect[]">
+                                    <option value="false">No</option>
+                                    <option value="true">Yes</option>
+                                </select>
+                            </div>
                         </div>
-                    </div>
-                    <div data-answer="1">
-                        <div class="form-group">
-                            <label for="">Anser 2:</label>
-                            <input type="text" class="form-control" required>
-                        </div>
-                        <label for="">Is correct?</label>
-                          <select class="form-control" name="isCorrect[]" >
-                            <option value="false">No</option>
-                            <option value="true">Yes</option>
-                        </select>
                     </div>
                 </div>
+                <button type="button" class="btn btn-success addAnswerBtn">Add Answer</button>
             </div>
-            <button type="button" class="btn btn-success addAnswerBtn">Add Answer</button>
         `);
 
         container.append(questionDiv);
     }
-    function addAnswer(questionNumber,answer) {
-        const answersContainer = $(`.answerContainer[data-answer-for-question="${questionNumber}"]`);
-        if(answersContainer.children().length < 6){
-            const answerInput = $(document.createElement('div')).attr({
-                class: "form-group",
-                'data-answer': answersContainer.children().length,
 
-            }).html(`
-               <div class="form-group">
-                   <label for="">Answer ${answersContainer.children().length+1}</label>
-                   <input type="text" class="form-control" required>
-               </div>
-               <div class="form-group">
-                   <label for="">Is correct?</label>
-                    <select class="form-control" name="isCorrect[]" >
-                        <option value="false">No</option>
-                        <option value="true">Yes</option>
-                    </select> 
-                </div>
-            `)
+    function addAnswer(questionNumber) {
+        const answersContainer = $(`.answerContainer[data-answer-for-question="${questionNumber}"]`);
+        const answerCounter = answersContainer.children().length;
+
+        if (answerCounter < 6) {
+            const answerInput = generateAnswerHTML(answerCounter + 1);
             answersContainer.append(answerInput);
         }
     }
 
-    document.getElementById("addQuestionBtn").addEventListener('click', addQuestion);
+    function generateAnswerHTML(answerNumber) {
+        return $(`
+            <div class="form-group" data-answer="${answerNumber}">
+                <label for="">Answer ${answerNumber}:</label>
+                <input type="text" class="form-control" data-answer-name required>
+                <label for="">Is correct?</label>
+                <select class="form-control" name="isCorrect[]">
+                    <option value="false">No</option>
+                    <option value="true">Yes</option>
+                </select>
+            </div>
+        `);
+    }
 
-    container.on('click', '.addAnswerBtn', function() {
-        const questionNumber = $(this).closest('div').find('input[data-question]').data('question');
-        addAnswer(questionNumber,2);
+    $('#addQuestionBtn').click(addQuestion);
+
+    container.on('click', '.addAnswerBtn', function () {
+        const questionNumber = $(this).closest('[data-question]').data('question');
+        addAnswer(questionNumber);
     });
 
-    $('#quizForm').submit(function(e) {
+    $('#quizForm').submit(function (e) {
         e.preventDefault();
 
-        const formData = $(this).serializeArray();
-        const jsonData = {};
+        const response = {
+            quizName: $('[data-name="quizName"]').val(),
+            quizIntroduction: $('[data-introduction="quizIntroduction"]').val(),
+            quizDurationTime: $('[data-duration="quizDurationTime"]').val(),
+            questions: [],
+        };
 
-        formData.forEach(function(field) {
-            jsonData[field.name] = field.value;
+        $('[data-question]').each(function () {
+            const questionName = $(this).find('[data-name-question]').val();
+            const questionDescription = $(this).find('[data-description-question]').val();
+            const questionMap = {
+                questionName,
+                questionDescription,
+                answers: [],
+            };
+
+            $(this).find('.answerContainer [data-answer]').each(function () {
+                const answerName = $(this).find('[data-answer-name]').val();
+                const isCorrect = $(this).find('select[name="isCorrect[]"]').val();
+                const answerMap = {
+                    answerName,
+                    isCorrect,
+                };
+                questionMap.answers.push(answerMap);
+            });
+
+            response.questions.push(questionMap);
         });
-        console.log(jsonData);
-        // $.ajax({
-        //   type: 'POST',
-        //   url: '/createQuiz',
-        //   contentType: 'application/json;charset=UTF-8',
-        //   data: JSON.stringify(jsonData),
-        //   success: function(response) {
-        //     // Обработка успешного ответа от бэкенда
-        //     console.log(response);
-        //   },
-        //   error: function(error) {
-        //     // Обработка ошибки
-        //     console.error(error);
-        //   }
-        // });
+// $.ajax({
+//         //   type: 'POST',
+//         //   url: '/createQuiz',
+//         //   contentType: 'application/json;charset=UTF-8',
+//         //   data: JSON.stringify(jsonData),
+//         //   success: function(response) {
+//         //     // Обработка успешного ответа от бэкенда
+//         //     console.log(response);
+//         //   },
+//         //   error: function(error) {
+//         //     // Обработка ошибки
+//         //     console.error(error);
+//         //   }
+//         // });
+        console.log(JSON.stringify(response));
     });
 });
